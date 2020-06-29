f883b34d58810c13b4eb7e721c305ba9f68f9c3d
rosenrose <42435748+rosenrose@users.noreply.github.com>
2020-06-29 18:36:21 +0900

    s

--- main.js
+++ main.js
@@ -141,10 +141,8 @@ document.querySelector("#run").addEventListener("click", function() {
 });
 
 function getData(url) {
-    return fetch(url,{
-        mode: 'no-cors'
-    })
-    .then(response => response.json());
+    return fetch(url)
+        .then(response => response.json());
 }
 
 function getRandomInt(minInclude, maxExclude) {
