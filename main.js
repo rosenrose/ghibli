b3b2712ca5bb23833a70986198ea00833a0959f3
rosenrose <42435748+rosenrose@users.noreply.github.com>
2020-07-03 07:21:26 +0900

    wait image loading

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-async function init() {
-    movieSelect = "list"
-    movie = -1;
-    userSelect = [];
-    count = 6;
-    list = await getData("./list.json");
-    document.querySelector("#loading").style.display = "none";
+format = "jpg";
+movieSelect = "list";
+movie = -1;
+userSelect = [];
+count = 6;
+fetch("./list.json").then(response => response.json())
+    .then(json => {
+    list = json;
+    toggleButton(document.querySelector("#run"));
 
     let movieList = document.querySelector("#movieList");
     let movieCheckbox = document.querySelectorAll("#movieCheckbox td");
@@ -34,24 +36,30 @@ async function init() {
         label.appendChild(document.createTextNode(name.slice(0,-7)));
         movieCheckbox[i].appendChild(label);
     }
+});
 
-    let result = document.querySelector("#result");
-    for (i=0; i<36; i++) {
-        let item = document.createElement("div");
-        item.className = "item";
-        let img = document.createElement("img");
-        img.src = "";
-        let title = document.createElement("p");
-        title.className = "shadow-white bold";
-        title.innerText = "";
-        item.appendChild(img);
-        item.appendChild(title);
-        result.appendChild(item);
-    }
+let result = document.querySelector("#result");
+for (i=0; i<36; i++) {
+    let item = document.createElement("div");
+    item.className = "item";
+    let img = document.createElement("img");
+    img.src = "";
+    let title = document.createElement("p");
+    title.className = "shadow-white bold";
+    title.innerText = "";
+    item.appendChild(img);
+    item.appendChild(title);
+    result.appendChild(item);
 }
-init();
 
-let radios = document.querySelectorAll("#movieSelect input[type='radio']");
+let radios = document.querySelectorAll("#formatSelect input");
+for (let radio of radios) {
+    radio.addEventListener("change", event => {
+        format = event.target.value;
+    });
+}
+
+radios = document.querySelectorAll("#movieSelect input[type='radio']");
 for (let radio of radios) {
     radio.addEventListener("change", event => {
         movieSelect = event.target.value;
@@ -93,18 +101,16 @@ for (let radio of radios) {
     });
 }
 
-document.querySelector("#run").addEventListener("click", function() {
+document.querySelector("#run").addEventListener("click", () => {
     if (typeof mutex == "undefined") {
         mutex = true;
     }
     if (mutex) {
         mutex = false;
-        let items = result.querySelectorAll(".item");
-        for (let i=count; i<items.length; i++) {
-            items[i].querySelector("img").src = "";
-            items[i].querySelector("p").innerText = "";
-        }
+        toggleButton(document.querySelector("#run"));
+        clear();
 
+        promises = [];
         for (let i=0; i<count; i++) {
             let randMovie;
             if (movieSelect == "list") {
@@ -123,12 +129,12 @@ document.querySelector("#run").addEventListener("click", function() {
                     randMovie = getRandomInt(0, list.movies.length);
                 }
             }
-
             let randCut = getRandomInt(0, list.movies[randMovie].cut.length);
-            let cut = list.movies[randMovie].cut[randCut].toString().padStart(5,"0");
-            
+
+            let cut = list.movies[randMovie].cut[randCut].toString().padStart(5,"0");            
             let title = list.movies[randMovie].name;
-            items[i].querySelector("img").src = `https://d2wwh0934dzo2k.cloudfront.net/ghibli/${title}/${cut}.jpg`;
+            let image = items[i].querySelector("img");
+            promises.push(loadImage(image, `https://d2wwh0934dzo2k.cloudfront.net/ghibli/${title}/${cut}.jpg`));
             if ((movieSelect == "list" && movie == -1) || (movieSelect == "checkbox" && userSelect.length != 1)) {
                 items[i].querySelector("p").innerText = title.slice(3,-7);
             }
@@ -136,33 +142,54 @@ document.querySelector("#run").addEventListener("click", function() {
                 items[i].querySelector("p").innerText = "";
             }
         }
-        mutex = true;
+        Promise.all(promises).then(() => {
+            mutex = true;
+            toggleButton(document.querySelector("#run"));
+        })
     }
 });
 
-document.querySelector("#gif").addEventListener("click", function() {
-    let randMovie = getRandomInt(0, list.movies.length);
-    let title = list.movies[randMovie].name;
-    let randCut = getRandomInt(0, list.movies[randMovie].cut.length-60);
-    let randCuts = list.movies[randMovie].cut.slice(randCut, randCut+60);
-    let cuts = [];
-    for (let cut of randCuts) {
-        cuts.push(cut.toString().padStart(5,"0"))
+// document.querySelector("#webp").addEventListener("click", () => {
+//     let randMovie = getRandomInt(0, list.movies.length);
+//     let title = list.movies[randMovie].name;
+//     let randCut = getRandomInt(0, list.movies[randMovie].cut.length-60);
+//     let randCuts = list.movies[randMovie].cut.slice(randCut, randCut+60);
+
+//     fetch("http://3.34.46.170:8080/webp", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: `count=${count}&title=${encodeURIComponent(title)}&cuts=${randCuts.toString()}`
+//     }).then(response => console.log(response));
+// });
+
+function loadImage(image, url) {
+    return new Promise(resolve => {
+        image.src = url;
+        image.addEventListener("load", () => {
+            resolve();
+        });        
+    });
+}
+
+function toggleButton(button) {
+    if (button.disabled) {
+        button.disabled = false;
+        button.innerText = "뽑기!";
     }
-    cuts = cuts.join(",")
+    else {
+        button.disabled = true;
+        button.innerText = "로딩...";
+    }
+}
 
-    fetch("http://3.34.46.170:8080/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: `title=${encodeURIComponent(title)}&cuts=${cuts}`
-    }).then(response => console.log(response));
-});
-
-function getData(url) {
-    return fetch(url)
-        .then(response => response.json());
+function clear() {
+    let items = result.querySelectorAll(".item");
+    for (let i=count; i<items.length; i++) {
+        items[i].querySelector("img").src = "";
+        items[i].querySelector("p").innerText = "";
+    }
 }
 
 function getRandomInt(minInclude, maxExclude) {
